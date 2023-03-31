af118736e898887c618405d7c11520f5f3d79261
JonasMht <Jonas.mht.private@gmail.com>
2023-03-31 12:27:10 +0200

    New Headless Server

---
+++ Distilling/headless_segmentation.py
@@ -0,0 +1,300 @@
+from __future__ import print_function
+# %matplotlib inline
+import argparse
+import os
+import os.path as osp
+import random
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import torch.utils.data as data
+from torch.autograd import Variable
+from torch.optim.lr_scheduler import StepLR
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
+import numpy as np
+from IPython.display import HTML
+from PIL import Image
+from tqdm import tqdm
+from torch import Tensor
+import pickle
+from datetime import datetime
+
+import copy # To make a copy of a model
+
+
+from torchvision import datasets, transforms
+from torch.utils.data import random_split
+
+from torchsummary import summary
+
+
+import utils
+from utils.dataset import *
+from utils.model import *
+from utils.model import UNet_modular
+from utils.utils import *
+import csv
+
+
+# Set random seed for reproducibility
+manualSeed = 999
+# manualSeed = random.randint(1, 10000) # use if you want new results
+print("Random Seed: ", manualSeed)
+random.seed(manualSeed)
+torch.manual_seed(manualSeed)
+
+
+"""
+# If there are issues with the kernel, try the following :
+
+
+python3 -m pip install ipykernel
+python3 -m ipykernel install --user
+
+python3 -m pip install ipykernel -U --user --force-reinstall
+
+python3.6 -m pip install torchvision
+
+
+# Run:
+~/env/bin/python headless_segmentation.py 
+"""
+
+# Glbal teacher model
+n_epochs = 1
+n_models = 20
+
+
+date = str(datetime.now()).split(' ')[0]
+heure = str(datetime.now()).split(' ')[1].split('.')[0]
+
+# Root directory for dataset
+# Refaire nos data folder et tout pour que ce soit
+# au format demandé par le dataloader
+
+# Leave empty if you want to use the default name or load a previous session
+session_name = ""
+
+if len(session_name) == 0:
+	session_name = "IGBMC_I3"+"_"+str(date)+"_"+str(heure)
+
+
+dataset_folder = "/home/mehtali/TER_CNN_Compression/Data/training-data/data/IGBMC_I3_diversifie/patches/"
+
+train_list = dataset_folder+"train_4000_i3.txt"
+teacher_train_list = dataset_folder+"train_10000_i3.txt"
+test_list = dataset_folder+"test_5000-7500_i3.txt"
+validate_list = dataset_folder+"test_1000.txt"
+
+# For debug
+"""
+train_list = dataset_folder+"test_20.txt"
+teacher_train_list = dataset_folder+"test_20.txt"
+test_list = dataset_folder+"test_20.txt"
+validate_list = dataset_folder+"test_20.txt"
+"""
+
+
+
+# Number of workers for dataloader
+workers = 10
+
+# Batch size during training (low batch_size if there are memory issues)
+batch_size = 10
+
+# Number of channels in the training images. For color images this is 3
+nc = 3
+
+
+
+# some net variable
+amp = False
+
+save_path =  "../Data/Saves/" + session_name+"/"
+model_path = save_path+"newtork_weigths/"
+log_path = save_path+"logs/"
+fig_path = save_path+"fig/"
+
+
+if not os.path.exists("../Data"):
+	os.mkdir("../Data")
+if not os.path.exists("../Data/Saves" ):
+	os.mkdir("../Data/Saves" )
+
+# We create this folder (only if it doesn't exists) to save weights of the training at some keys epoch
+if not os.path.exists(save_path):
+	os.mkdir(save_path)
+	os.mkdir(model_path)
+	os.mkdir(log_path)
+	os.mkdir(fig_path)
+
+
+log_file = open(log_path+"log.txt", "w")
+
+log_file.write("dataset_folder :"+dataset_folder+"\n")
+log_file.write("batch_size="+str(batch_size)+"\n")
+log_file.write("num_max_epoch="+str(n_epochs)+"\n")
+log_file.write("nc="+str(nc)+"\n")
+log_file.close()
+
+print("number of gpus :", torch.cuda.device_count())
+
+# Decide which device we want to run on
+device = torch.device("cuda:0" if (
+	torch.cuda.is_available() and torch.cuda.device_count() > 0) else "cpu")
+
+# On regarde l'identifiant du GPU ou CPU sur lequel on travaille
+print("device ID", device)
+print("nom du GPU", torch.cuda.get_device_name(device))  # On vérifie son "nom"
+
+torch.cuda.empty_cache()
+
+
+
+
+# For data augmentation
+
+geometric_augs = [
+	# transforms.Resize((256, 256)), # Makes it easier to process using net
+	# transforms.RandomRotation(degrees=(0, 180)),
+	transforms.RandomHorizontalFlip(),
+	transforms.RandomVerticalFlip(),
+	# transforms.RandomRotation(45),
+]
+
+color_augs = [
+	# transforms.ColorJitter(hue=0.05, saturation=0.4)
+]
+
+
+def make_tfs(augs):
+	return transforms.Compose([transforms.ToPILImage()]+augs + [transforms.ToTensor()])
+
+
+tfs = transforms.Compose(geometric_augs)
+
+
+# Importation des images et masques de i3
+train_dataset = SegmentationDataSet(root=dataset_folder,
+							  list_path=train_list,
+							  transform_img=make_tfs(
+								  geometric_augs + color_augs),
+							  transform_label=make_tfs(geometric_augs)
+							  )
+
+teacher_train_dataset = SegmentationDataSet(root=dataset_folder,
+							  list_path=teacher_train_list,
+							  transform_img=make_tfs(
+								  geometric_augs + color_augs),
+							  transform_label=make_tfs(geometric_augs)
+							  )
+
+# Not a good idea to augment the test data
+test_dataset = SegmentationDataSet(root=dataset_folder,
+								list_path=test_list
+								)
+# Not a good idea to augment the test data
+validate_dataset = SegmentationDataSet(root=dataset_folder,
+								list_path=validate_list
+								)
+
+
+
+train_dataloader = torch.utils.data.DataLoader(train_dataset,
+											   batch_size=batch_size,
+											   shuffle=True,
+											   num_workers=workers)
+
+teacher_train_dataloader= torch.utils.data.DataLoader(teacher_train_dataset,
+											   batch_size=batch_size,
+											   shuffle=True,
+											   num_workers=workers)
+
+test_dataloader = torch.utils.data.DataLoader(test_dataset,
+											  batch_size=batch_size,
+											  shuffle=True,
+											  num_workers=workers)
+
+validation_dataloader = torch.utils.data.DataLoader(validate_dataset,
+											  batch_size=batch_size,
+											  shuffle=True,
+											  num_workers=workers)
+
+
+batch = next(iter(train_dataloader))
+
+# On affiche quelques exemple du batch pour vérifier qu'on a bien importé les données
+print("images source : ", batch[0].shape)
+print("mask source :", batch[1].shape)
+
+
+
+
+
+# Training
+
+log_file = open(log_path+"model_performance.txt", "w")
+log_file.write("Best Dice\tModel Name\n")
+log_file.flush()
+
+# Teacher model
+teacher_model = load_model(UNet_modular(channel_depth=32, n_channels=3, n_classes=1), device)
+model_descr = "teacher_model_param_{}".format(get_trainable_param(teacher_model))
+teacher_load_path = ""
+if teacher_load_path != "":
+	teacher_model.load_state_dict(torch.load(teacher_load_path))
+else:
+	# Train teacher Model
+	teacher_model, best_dice = model_training(teacher_model, teacher_train_dataloader, validation_dataloader, train, evaluate, n_epochs)
+	teacher_model_path = model_path+"teacher/"
+	if not os.path.exists(teacher_model_path):
+		os.mkdir(teacher_model_path)
+	save_model(teacher_model, teacher_model_path+model_descr+".pth")
+	log_file.write("Dice : {:.3f}\tModel : {}\n".format(best_dice, model_descr))
+	log_file.flush()
+
+# Train smaller models
+for i in range(1, n_models+1):
+	d_model_path = model_path+"d_model_"+str(i)+"/"
+	nd_model_path = model_path+"nd_model_"+str(i)+"/"
+	
+	if not os.path.exists(nd_model_path):
+		os.mkdir(nd_model_path)
+	if not os.path.exists(d_model_path):
+		os.mkdir(d_model_path)
+
+	for j in range(10):
+		d_model_iter_folder = d_model_path+"iter_"+str(j)+"/"
+		nd_model_iter_folder = nd_model_path+"iter_"+str(j)+"/"
+
+		if not os.path.exists(d_model_iter_folder):
+			os.mkdir(d_model_iter_folder)
+		if not os.path.exists(nd_model_iter_folder):
+			os.mkdir(nd_model_iter_folder)
+
+		# Train 10 models per depth modification
+
+		# Distilled model
+		model = load_model(UNet_modular(channel_depth=2*i, n_channels=3, n_classes=1), device)
+		model_descr = "d_model_{}_iteration_{}_param_{}".format(i, j, get_trainable_param(model))
+		nd_model, best_dice = model_training(model, train_dataloader, validation_dataloader, train_distilled, evaluate, n_epochs, other_model=teacher_model)
+		save_model(model, d_model_iter_folder+model_descr+".pth")
+		log_file.write("Dice : {:.3f}\tModel : {}\n".format(best_dice, model_descr))
+		log_file.flush()
+
+
+		# Not Distilled model
+		model = load_model(UNet_modular(channel_depth=2*i, n_channels=3, n_classes=1), device)
+		model_descr = "nd_model_{}_iteration_{}_param_{}".format(i, j, get_trainable_param(model))
+		d_model, best_dice = model_training(model, train_dataloader, validation_dataloader, train, evaluate, n_epochs)
+		save_model(model, nd_model_iter_folder+model_descr+".pth")
+		log_file.write("Dice : {:.3f}\tModel : {}\n".format(best_dice, model_descr))
+		log_file.flush()
+
+
+log_file.close()
--- Distilling/utils/utils.py
+++ Distilling/utils/utils.py
@@ -11,6 +11,8 @@ import torch.nn.functional as F
 import torch.nn.parallel
 import torch.backends.cudnn as cudnn
 import torch.optim as optim
+from torch.optim.lr_scheduler import StepLR
+from torch.autograd import Variable
 import torch.utils.data as data
 import torchvision.datasets as dset
 import torchvision.transforms as transforms
@@ -23,7 +25,7 @@ from PIL import Image
 from tqdm import tqdm
 from torch import Tensor
 from datetime import datetime
-
+import copy # To make a copy of a model
 
 def preprocessing(img, mask, device, crop=False):
     if not isinstance(img, np.ndarray):
@@ -130,203 +132,225 @@ def smooth(scalars, weight):  # Weight between 0 and 1
     return smoothed
 
 
-def trainStep(network, criterion, optimizer, X, y):
-    """
-    One training step of the network: forward prop + backprop + update parameters
-    Return: (loss, accuracy) of current batch
-    """
-    optimizer.zero_grad()
-    outputs = network(X)
-    loss = criterion(outputs, y)
-    loss.backward()
-    optimizer.step()
-    accuracy = float(torch.sum(torch.argmax(
-        outputs, dim=1) == y).item()) / y.shape[0]
-    return loss, accuracy
+# My utils
+
+def load_model(model, device, path=""):
+	ngpu = torch.cuda.device_count()
+	if len(path)>0:
+		state_dict = torch.load(path)
+		model.load_state_dict(state_dict)
+	model.to(device=device)
+
+	if (device.type == 'cuda'):
+		print("Data Parallel")
+		model = nn.DataParallel(model, list(range(ngpu)))
+	
+	return model
 
 
-def getLossAccuracyOnDataset(network, dataset_loader, fast_device, criterion=None):
-    """
-    Returns (loss, accuracy) of network on given dataset
-    """
-    network.is_training = False
-    accuracy = 0.0
-    loss = 0.0
-    dataset_size = 0
-    for j, D in enumerate(dataset_loader, 0):
-        X, y = D
-        X = X.to(fast_device)
-        y = y.to(fast_device)
-        with torch.no_grad():
-            pred = network(X)
-            if criterion is not None:
-                loss += criterion(pred, y) * y.shape[0]
-            accuracy += torch.sum(torch.argmax(pred, dim=1) == y).item()
-        dataset_size += y.shape[0]
-    loss, accuracy = loss / dataset_size, accuracy / dataset_size
-    network.is_training = True
-    return loss, accuracy
+# Other
+
+def save_model(model, path):
+	torch.save(model.state_dict(), path)
+
+# Function that returns a list of paths in a folder that start with a word and that are sorted in increasing order
+def get_path_list(path, start_with=""):
+	path_list = []
+	for file in os.listdir(path):
+		if file.startswith(start_with):
+			path_list.append(os.path.join(path, file))
+	path_list.sort(key=lambda f: int(''.join(filter(str.isdigit, f))))
+	return path_list
+# Function that returns in a path list with the first number in the name
+
+def get_trainable_param(model):
+	model_parameters = filter(lambda p: p.requires_grad, model.parameters())
+	return sum([np.prod(p.size()) for p in model_parameters])
 
 
-def trainTeacherOnHparam(teacher_net, hparam, num_epochs,
-                         train_loader, val_loader,
-                         print_every=0,
-                         fast_device=torch.device('cpu')):
-    """
-    Trains teacher on given hyperparameters for given number of epochs; Pass val_loader=None when not required to validate for every epoch 
-    Return: List of training loss, accuracy for each update calculated only on the batch; List of validation loss, accuracy for each epoch
-    """
-    train_loss_list, train_acc_list, val_loss_list, val_acc_list = [], [], [], []
-    teacher_net.dropout_input = hparam['dropout_input']
-    teacher_net.dropout_hidden = hparam['dropout_hidden']
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(teacher_net.parameters(
-    ), lr=hparam['lr'], momentum=hparam['momentum'], weight_decay=hparam['weight_decay'])
-    lr_scheduler = optim.lr_scheduler.StepLR(
-        optimizer, step_size=1, gamma=hparam['lr_decay'])
-    for epoch in range(num_epochs):
-        lr_scheduler.step()
-        if epoch == 0:
-            if val_loader is not None:
-                val_loss, val_acc = getLossAccuracyOnDataset(
-                    teacher_net, val_loader, fast_device, criterion)
-                val_loss_list.append(val_loss)
-                val_acc_list.append(val_acc)
-                print('epoch: %d validation loss: %.3f validation accuracy: %.3f' % (
-                    epoch, val_loss, val_acc))
-        for i, data in enumerate(train_loader, 0):
-            X, y = data
-            X, y = X.to(fast_device), y.to(fast_device)
-            loss, acc = trainStep(teacher_net, criterion, optimizer, X, y)
-            train_loss_list.append(loss)
-            train_acc_list.append(acc)
+def show_prediction(model, val_loader, n=0):
+	model.eval().cuda()
 
-            if print_every > 0 and i % print_every == print_every - 1:
-                print('[%d, %5d/%5d] train loss: %.3f train accuracy: %.3f' %
-                      (epoch + 1, i + 1, len(train_loader), loss, acc))
+	with torch.no_grad():
+		for i,(img,gt) in enumerate(val_loader):
+			if n !=0 and i > n:
+				break
+		
+			if torch.cuda.is_available():
+				img, gt = img.cuda(), gt.cuda()
+			img, gt = Variable(img), Variable(gt)
 
-        if val_loader is not None:
-            val_loss, val_acc = getLossAccuracyOnDataset(
-                teacher_net, val_loader, fast_device, criterion)
-            val_loss_list.append(val_loss)
-            val_acc_list.append(val_acc)
-            print('epoch: %d validation loss: %.3f validation accuracy: %.3f' %
-                  (epoch + 1, val_loss, val_acc))
-    return {'train_loss': train_loss_list,
-            'train_acc': train_acc_list,
-            'val_loss': val_loss_list,
-            'val_acc': val_acc_list}
+			output = model(img)
+			output = output.clamp(min = 0, max = 1)
+			gt = gt.clamp(min = 0, max = 1)
+			
 
+			# Plot image, output and ground truth one one row
+			fig, ax = plt.subplots(1,3, figsize=(15,15))
+			ax[0].imshow(img[0].cpu().numpy().transpose(1,2,0))
+			ax[1].imshow(output[0].cpu().numpy().transpose(1,2,0))
+			ax[2].imshow(gt[0].cpu().numpy().transpose(1,2,0))
+			# Name each axis
+			ax[0].set_title("Image")
+			ax[1].set_title("Output")
+			ax[2].set_title("Ground Truth")
+			
+			plt.show()
+	
 
-def studentTrainStep(teacher_net, student_net, studentLossFn, optimizer, X, y, T, alpha):
-    """
-    One training step of student network: forward prop + backprop + update parameters
-    Return: (loss, accuracy) of current batch
-    """
-    optimizer.zero_grad()
-    teacher_pred = None
-    if (alpha > 0):
-        with torch.no_grad():
-            teacher_pred = teacher_net(X)
-    student_pred = student_net(X)
-    loss = studentLossFn(teacher_pred, student_pred, y, T, alpha)
-    loss.backward()
-    optimizer.step()
-    accuracy = float(torch.sum(torch.argmax(
-        student_pred, dim=1) == y).item()) / y.shape[0]
-    return loss, accuracy
+# Training and testing
+
+def evaluate(model, val_loader):
+	model.eval().cuda()
+
+	dl = []
+	with torch.no_grad():
+		for i,(img,gt) in enumerate(val_loader):
+			if torch.cuda.is_available():
+				img, gt = img.cuda(), gt.cuda()
+			img, gt = Variable(img), Variable(gt)
+
+			output = model(img)
+			output = output.clamp(min = 0, max = 1)
+			gt = gt.clamp(min = 0, max = 1)
+			loss = dice_loss(output, gt)
+			dice = dice_coeff(output, gt)
+			dl.append(dice.item())
+
+	
+	mean_dice = np.mean(dl)
+	print("Eval metrics : dice {:.3f}.".format(mean_dice))
+	return mean_dice
 
 
-def trainStudentOnHparam(teacher_net, student_net, hparam, num_epochs,
-                         train_loader, val_loader,
-                         print_every=0,
-                         fast_device=torch.device('cpu')):
-    """
-    Trains teacher on given hyperparameters for given number of epochs; Pass val_loader=None when not required to validate for every epoch
-    Return: List of training loss, accuracy for each update calculated only on the batch; List of validation loss, accuracy for each epoch
-    """
-    train_loss_list, train_acc_list, val_acc_list = [], [], []
-    T = hparam['T']
-    alpha = hparam['alpha']
-    student_net.dropout_input = hparam['dropout_input']
-    student_net.dropout_hidden = hparam['dropout_hidden']
-    optimizer = optim.SGD(student_net.parameters(
-    ), lr=hparam['lr'], momentum=hparam['momentum'], weight_decay=hparam['weight_decay'])
-    lr_scheduler = optim.lr_scheduler.StepLR(
-        optimizer, step_size=1, gamma=hparam['lr_decay'])
+def train(model, optimizer, train_loader, other_model=None):
+	model.train().cuda()
+	dl = []
+	with tqdm(total=len(train_loader)*train_loader.batch_size, desc=f'Training', unit='img') as pbar:
+		for i, (img, gt) in enumerate(train_loader):
+			#print('i', i)
+			if torch.cuda.is_available():
+				img, gt = img.cuda(), gt.cuda()
+			
+			img, gt = Variable(img), Variable(gt)
 
-    def studentLossFn(teacher_pred, student_pred, y, T, alpha):
-        """
-        Loss function for student network: Loss = alpha * (distillation loss with soft-target) + (1 - alpha) * (cross-entropy loss with true label)
-        Return: loss
-        """
-        if (alpha > 0):
-            loss = F.kl_div(F.log_softmax(student_pred / T, dim=1), F.softmax(teacher_pred / T, dim=1),
-                            reduction='batchmean') * (T ** 2) * alpha + F.cross_entropy(student_pred, y) * (1 - alpha)
-        else:
-            loss = F.cross_entropy(student_pred, y)
-        return loss
+			output = model(img)
+			output = output.clamp(min = 0, max = 1)
+			gt = gt.clamp(min = 0, max = 1)
+			loss = dice_loss(output, gt)
+			dice = dice_coeff(output, gt)
+			dl.append(dice.item())
 
-    for epoch in range(num_epochs):
-        lr_scheduler.step()
-        if epoch == 0:
-            if val_loader is not None:
-                _, val_acc = getLossAccuracyOnDataset(
-                    student_net, val_loader, fast_device)
-                val_acc_list.append(val_acc)
-                print('epoch: %d validation accuracy: %.3f' % (epoch, val_acc))
-        for i, data in enumerate(train_loader, 0):
-            X, y = data
-            X, y = X.to(fast_device), y.to(fast_device)
-            loss, acc = studentTrainStep(
-                teacher_net, student_net, studentLossFn, optimizer, X, y, T, alpha)
-            train_loss_list.append(loss)
-            train_acc_list.append(acc)
+			
+			optimizer.zero_grad()
+			loss.backward()
 
-            if print_every > 0 and i % print_every == print_every - 1:
-                print('[%d, %5d/%5d] train loss: %.3f train accuracy: %.3f' %
-                      (epoch + 1, i + 1, len(train_loader), loss, acc))
+			optimizer.step()
 
-        if val_loader is not None:
-            _, val_acc = getLossAccuracyOnDataset(
-                student_net, val_loader, fast_device)
-            val_acc_list.append(val_acc)
-            print('epoch: %d validation accuracy: %.3f' % (epoch + 1, val_acc))
-    return {'train_loss': train_loss_list,
-            'train_acc': train_acc_list,
-            'val_acc': val_acc_list}
+			pbar.update(len(img))
+			pbar.set_postfix(**{'loss': loss.item(), "dice": dice.item()})
+	
+	mean_dice = np.mean(dl)
+
+	print("Average dice {:.3f} over this epoch.".format(mean_dice))
+
+	return mean_dice
 
 
-def hparamToString(hparam):
-    """
-    Convert hparam dictionary to string with deterministic order of attribute of hparam in output string
-    """
-    hparam_str = ''
-    for k, v in sorted(hparam.items()):
-        hparam_str += k + '=' + str(v) + ', '
-    return hparam_str[:-2]
+# Training function where the teacher model is used to generate soft targets in the case of image segmentation
+def train_distilled(model, optimizer, train_loader, other_model=None):
+	T = 1  # temperature for distillation loss
+	# Using a higher value for T produces a softer probability distribution over classes
+	alpha = 0.95
+	# trade-off between soft-target (st) cross-entropy and true-target (tt) cross-entropy;
+	# loss = alpha * st + (1 - alpha) * tt
 
+	teacher_model = other_model
 
-def hparamDictToTuple(hparam):
-    """
-    Convert hparam dictionary to tuple with deterministic order of attribute of hparam in output tuple
-    """
-    hparam_tuple = [v for k, v in sorted(hparam.items())]
-    return tuple(hparam_tuple)
+	model.train().cuda()
+	dl = []
+	with tqdm(total=len(train_loader)*train_loader.batch_size, desc=f'Training', unit='img') as pbar:
+		for i, (img, gt) in enumerate(train_loader):
+			#print('i', i)
+			if torch.cuda.is_available():
+				img, gt = img.cuda(), gt.cuda()
+			
+			img, gt = Variable(img), Variable(gt)
 
+			output = model(img)
+			output = output.clamp(min = 0, max = 1)
+			gt = gt.clamp(min = 0, max = 1)
+			loss = dice_loss(output, gt)
+			dice = dice_coeff(output, gt)
+			dl.append(dice.item())
 
-def getTrainMetricPerEpoch(train_metric, updates_per_epoch):
-    """
-    Smooth the training metric calculated for each batch of training set by averaging over batches in an epoch
-    Input: List of training metric calculated for each batch
-    Output: List of training matric averaged over each epoch
-    """
-    train_metric_per_epoch = []
-    temp_sum = 0.0
-    for i in range(len(train_metric)):
-        temp_sum += train_metric[i]
-        if (i % updates_per_epoch == updates_per_epoch - 1):
-            train_metric_per_epoch.append(temp_sum / updates_per_epoch)
-            temp_sum = 0.0
+			# Compute soft targets using the teacher model
+			with torch.no_grad():
+				soft_targets = teacher_model(img)
+				soft_targets = soft_targets.clamp(min = 0, max = 1)
+				soft_targets = F.softmax(soft_targets/T, dim=1)
 
-    return train_metric_per_epoch
+			# Compute the true targets
+			true_targets = gt
+
+			# Compute the distillation loss
+			distillation_loss = dice_loss(F.softmax(output/T, dim=1), soft_targets) * (T * T) * alpha + loss * (1. - alpha)
+
+			optimizer.zero_grad()
+			distillation_loss.backward()
+
+			optimizer.step()
+
+			pbar.update(len(img))
+			pbar.set_postfix(**{'loss': distillation_loss.item(), "dice": dice.item()})
+
+	mean_dice = np.mean(dl)
+
+	print("Average dice {:.3f} over this epoch.".format(mean_dice))
+
+	return mean_dice
+
+def model_training(model, train_loader, val_loader, trainFun, evalFun, n_epochs, name="", other_model=None):
+	optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+	scheduler = StepLR(optimizer, step_size = 100, gamma = 0.2)
+
+	former_best_dice = 0 # Worst possible
+	early_stopping_streak = 0
+
+	scheduled_saves = 0
+	best_model = model
+
+	for epoch in range(n_epochs):
+		print(" --- training: epoch {}".format(epoch+1))
+		# Train the model
+		train_dice = trainFun(model, optimizer, train_loader, other_model)
+
+		# Modulate training rate
+		scheduler.step()
+
+		#evaluate for one epoch on validation set
+		valid_dice = evalFun(model, val_loader)
+		
+		#if val_metric is best, add checkpoint
+		if (epoch%10 == 0 or epoch==(n_epochs-1)):
+			scheduled_saves+=1
+		
+
+		if former_best_dice < valid_dice:
+			# dice is increasing
+			early_stopping_streak = 0
+			best_model = copy.deepcopy(model)
+			if name != "" and scheduled_saves > 0:
+				scheduled_saves -= 1
+				save_model(model, model_path+name+"_dice_{:.3f}_loss_{:.3f}_epoch_{}.pth".format(valid_dice, valid_loss, epoch+1))
+		else:
+			# Is decresing
+			early_stopping_streak += 1
+			if early_stopping_streak >= 20:
+				# 15 consecutive epochs without improvement in dice
+				print("Early stopping at epoch {}".format(epoch+1))
+				break
+		
+		former_best_dice = max(former_best_dice, valid_dice)
+		
+	return (best_model, former_best_dice)
